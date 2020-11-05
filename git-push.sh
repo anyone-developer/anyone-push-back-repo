259e9cd1922a6ceb2c198439ff3e34f2010f886d
nzhang4 <nzhang4@stubhub.com>
2020-11-05 15:39:58 +0800

    test

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 if [ $1 == 'true' ]; then
   git config --local user.name $2
   git config --local user.email $3
-  git commit -am "Automated push from workflow: $4"
+  git commit -am "Automated push from [$4/#$5]"
   git push
 fi
