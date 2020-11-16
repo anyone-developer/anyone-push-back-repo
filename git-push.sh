32c14eb1d38c41c8dd2476d375db1fe4ebce8b40
nzhang4 <nzhang4@stubhub.com>
2020-11-16 18:23:55 +0800

    add gpg signing key generator

--- git-push.sh
+++ git-push.sh
@@ -7,6 +7,7 @@ if [[ $1 == 1 ]]; then
   git checkout "$5"
   git rebase $6 -X$7
   git add --all
+  git config --global user.signingkey $8
   git commit -am "Automated push from [$3/#$4]"
   git push
 fi
