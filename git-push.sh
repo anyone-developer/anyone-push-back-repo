69725b0c52099ef636f70e56b94e98a544c4f80d
nzhang4 <nzhang4@stubhub.com>
2020-11-17 16:08:36 +0800

    fix
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    fix
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    fix
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    update
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    update
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    update
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    update
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    update
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>
    
    Update git-push.sh
    
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>

--- git-push.sh
+++ git-push.sh
@@ -3,10 +3,13 @@
 if [[ $1 == 1 ]]; then
   git config --local user.name $2
   git config --local user.email $3
+  
   git fetch
-  git checkout "$6"
-  git rebase $7 -X$8
-  git add --all
-  git commit -am "Automated push from [$4/#$5]"
-  git push
+  git checkout $4
+  git pull --ff-only origin $4
+  git rebase $5 -X $6
+  
+  git add .
+  git commit -a -m "Automated push for workflow: [$7/#$8]"
+  git push origin HEAD:$4
 fi
