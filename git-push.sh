c417162516610877019cf6db1c025959407ca8ae
nzhang4 <nzhang4@stubhub.com>
2020-11-18 13:16:21 +0800

    fix
    Co-Authored-By: Tekdycaml <zhang_nan_163@163.com>

--- git-push.sh
+++ git-push.sh
@@ -5,11 +5,18 @@ if [[ $1 == 1 ]]; then
   git config --local user.email $3
   
   git fetch
-  git checkout $4
-  git pull --ff-only origin $4
-  git rebase $5 -X $6
-  
-  git add .
-  git commit -a -m "Automated push for workflow: [$7/#$8]"
-  git push origin HEAD:$4
+
+  if [[ $4 == 0]] || [[ $5 == 0 ]]; then
+    git add .
+    git commit -a -m "Automated push for workflow: [$8/#$9]"
+    git push origin $6
+  else
+    git checkout $4
+    git pull --ff-only origin $4
+    git rebase $5 -X $7
+    git add .
+    git commit -a -m "Automated push for workflow: [$8/#$9]"
+    git push origin HEAD:$4
+  fi
+
 fi
