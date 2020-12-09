5f20f046d8a0d6f747382aa4e0f9ffc10f49c2f1
nzhang4 <nzhang4@stubhub.com>
2020-12-09 18:29:55 +0800

    update code

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-. ./git-status.sh
+actor=$1
+actor_email=$2
+head_ref=$3
+base_ref=$4
+ref=$5
+strategy_option=$6
+workflow=$7
+run_number$8
+
+source ./git-status.sh
 
 print=`GET_GIT_STATUS`
 modified=$?
@@ -8,19 +17,19 @@ modified=$?
 echo "$print"
 echo ::set-output name=modified::$modified #set output
 
-if [[ $modified != 0 ]]; then
-  echo "[INFO] set user.name to: $1"
-  git config --local user.name $1
-  echo "[INFO] set user.email to: $2"
-  git config --local user.email $2
+if [[ $modified == 1 ]]; then
+  echo "[INFO] set user.name to: $actor"
+  git config --local user.name $actor
+  echo "[INFO] set user.email to: $actor_email"
+  git config --local user.email $actor_email
   
   echo "[INFO] ready to git fetch"
   git fetch
 
-  branch=$(echo $5 | cut -d "/" -f 3) #direct to push
+  branch=$(echo $ref | cut -d "/" -f 3) #direct to push
 
-  if [[ $3 != "" ]] && [[ $4 != "" ]]; then
-    branch=$3 #pull request source branch, will rebase to target branch
+  if [[ $head_ref != "" ]] && [[ $base_ref != "" ]]; then
+    branch=$head_ref #pull request source branch, will rebase to target branch
   fi
 
   echo "[INFO] checkout branch: $branch"
@@ -29,19 +38,19 @@ if [[ $modified != 0 ]]; then
   echo "[INFO] pull --ff-only from origin $branch"
   git pull --ff-only origin $branch
 
-  if [[ $3 != "" ]] && [[ $4 != "" ]]; then
-    echo "[INFO] rebase from $4 with strategy $6"
-    git rebase $4 -X $6
+  if [[ $head_ref != "" ]] && [[ $base_ref != "" ]]; then
+    echo "[INFO] rebase from $base_ref with strategy $strategy_option"
+    git rebase $base_ref -X $strategy_option
   fi
 
   git add .
   message="Automated push for workflow"
-  if [[ $7 != "" ]] && [[ $8 != "" ]]; then
-    message="Automated push for workflow: [$7/#$8]"
+  if [[ $workflow != "" ]] && [[ $run_number != "" ]]; then
+    message="Automated push for workflow: [$workflow/#$run_number]"
   fi
   git commit -a -m "$message"
-  echo "[INFO] push to: $3"
-  git push origin $3
+  echo "[INFO] push to: $branch"
+  git push origin $branch
 fi
 
 exit 0
