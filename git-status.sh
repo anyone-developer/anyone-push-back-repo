e69740078bd5d984449da7212d059df28a322986
nzhang4 <nzhang4@stubhub.com>
2020-12-10 13:19:07 +0800

    fix

--- git-status.sh
+++ git-status.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-tracked=$(git status -sb | tail -n +2)
-hehind=$(git status -sb | sed -n 1p | grep behind)
-ahead=$(git status -sb | sed -n 1p | grep ahead)
+function GET_GIT_STATUS()
+{
+  tracked=$(git status -sb | tail -n +2)
+  hehind=$(git status -sb | sed -n 1p | grep behind)
+  ahead=$(git status -sb | sed -n 1p | grep ahead)
 
-if [[ "$tracked" == "" ]] && [[ "$behind" == "" ]] && [[ "$ahead" == "" ]]; then
-  echo "[INFO] There is no any changes or commits behind/ahead to the head of branch"
-  exit 0
-else
-  if [[ "$tracked" != "" ]]; then
-    echo "[INFO] track files:"
-    echo "$tracked"
+  if [[ "$tracked" == "" ]] && [[ "$behind" == "" ]] && [[ "$ahead" == "" ]]; then
+    echo "[INFO] There is no any changes or commits behind/ahead to the head of branch"
+    return 0
+  else
+    if [[ "$tracked" != "" ]]; then
+      echo "[INFO] track files:"
+      echo "$tracked"
+    fi
+
+    if [[ "$behind" != "" ]]; then
+      echo "[INFO] branch behind:"
+      echo $(git status -sb | sed -n 1p)
+    fi
+
+    if [[ "$ahead" != "" ]]; then
+      echo "[INFO] branch ahead:"
+      echo $(git status -sb | sed -n 1p)
+    fi
+
+    return 1
   fi
+}
 
-  if [[ "$behind" != "" ]]; then
-    echo "[INFO] branch behind:"
-    echo $(git status -sb | sed -n 1p)
-  fi
-
-  if [[ "$ahead" != "" ]]; then
-    echo "[INFO] branch ahead:"
-    echo $(git status -sb | sed -n 1p)
-  fi
-
-  exit 1
-fi
+print=`GET_GIT_STATUS`
+echo ::set-output name=modified::$?
+echo "$print"
