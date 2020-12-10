adaca0e5567774327eab5849bc5b6fdfab0e71ef
nzhang4 <nzhang4@stubhub.com>
2020-12-10 10:58:03 +0800

    fix

--- git-action.sh
+++ git-action.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-input_actor=$(if [[ "${{ inputs.actor }}" == "" ]]; then echo 0; else echo "${{ inputs.actor }}"; fi)
-input_actor_email=$(if [[ "${{ inputs.actor-email }}" == "" ]]; then echo 0; else echo "${{ inputs.actor-email }}"; fi)
-github_head_ref=$(if [[ "${{github.head_ref}}" == "" ]]; then echo 0; else echo "${{github.head_ref}}"; fi)
-github_base_ref=$(if [[ "${{github.base_ref}}" == "" ]]; then echo 0; else echo "${{github.base_ref}}"; fi)
-github_ref=$(if [[ "${{github.ref}}" == "" ]]; then echo 0; else echo "${{github.ref}}"; fi)
-input_strategy_option=$(if [[ "${{ inputs.strategy-option }}" == "" ]]; then echo 0; else echo "${{ inputs.strategy-option }}"; fi)
-github_workflow= $(if [[ "${{github.workflow}}" == "" ]]; then echo 0; else echo "${{github.workflow}}"; fi)
-github_row_number=$(if [[ "${{github.run_number}}" == "" ]]; then echo 0; else echo "${{github.run_number}}"; fi)
+input_actor=$(if [[ "$1" == "" ]]; then echo 0; else echo "$1"; fi)
+input_actor_email=$(if [[ "$2" == "" ]]; then echo 0; else echo "$2"; fi)
+github_head_ref=$(if [[ "$3" == "" ]]; then echo 0; else echo "$3"; fi)
+github_base_ref=$(if [[ "$4" == "" ]]; then echo 0; else echo "$4"; fi)
+github_ref=$(if [[ "$5" == "" ]]; then echo 0; else echo "$5"; fi)
+input_strategy_option=$(if [[ "$6" == "" ]]; then echo 0; else echo "$6"; fi)
+github_workflow= $(if [[ "$7" == "" ]]; then echo 0; else echo "$7"; fi)
+github_row_number=$(if [[ "$8" == "" ]]; then echo 0; else echo "$8"; fi)
 
 sh ./git-push.sh $input_actor $input_actor_email $github_head_ref $github_base_ref $github_ref $input_strategy_option $github_workflow $github_row_number
